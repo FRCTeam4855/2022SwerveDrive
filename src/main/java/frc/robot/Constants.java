93387e2da1cdfcfda2c4f75a3409653ba55435e4
Philip Kerber <philip.kerber22@gmail.com>
2022-03-05 15:13:40 -0500

    Moved and Tidied

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,6 +2,7 @@ package frc.robot;
 
 public class Constants {
     //Constants
+    public static final double JOYSTK_DZONE = .17; //global deadzone for all controllers
 
     // Conversion 
     public static final double RELATIVE_ENC_TO_FT = 7.4; //0.660715;//7.928591; //0.03666658; // converts relative encoder units on neo to inches
@@ -12,8 +13,9 @@ public class Constants {
     public static final int ORIENTATION_TOGGLE = 3; 
 
 
+
     // Operator Mappings
-    public static final int CLIMBER_TOGGLE = 3;
+    public static final int CLIMBERARM_TOGGLE = 3;
     public static final int FLYWHEEL_TOGGLE = 1;
     public static final int INTAKEARM_TOGGLE = 2;
 
